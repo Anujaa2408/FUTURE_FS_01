dac81845aba078de07d8a252e7466b155c507288
Anujaa2408 <anujaac0624@gmail.com>
2025-12-29 20:12:45 +0530

    Update script.js

--- Task_2_Ecommerce/script.js
+++ Task_2_Ecommerce/script.js
@@ -1,1 +1,13 @@
+let cartCount = 0;
+let totalPrice = 0;
+
+document.querySelectorAll(".add-to-cart").forEach(button => {
+    button.addEventListener("click", () => {
+        cartCount++;
+        totalPrice += Number(button.dataset.price);
+
+        document.getElementById("cart-count").innerText = cartCount;
+        document.getElementById("total-price").innerText = totalPrice;
+    });
+});
 
